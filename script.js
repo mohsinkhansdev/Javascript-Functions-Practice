802a22c83f34b51983915bb36f9c98e1af50b379
Muhammad Mohsin Khan <77793691+mohsinkhansdev@users.noreply.github.com>
2022-02-07 18:56:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+// JavaScript Functions Practice
+
+
+
+
+
+/* function kebabToSnake(str) {
+    // replace all '-' with "_"
+    var newstr = str.replace(/-/g,"_");
+    //return str
+    return newstr;
+}
+
+kebabToSnake("hello-world"); */
+
+/* function factorial(x){
+    var r = 1;
+    for(var i = 1; i <= x;  i++) {
+        r = r * i;
+    }
+    r *=i;
+}
+
+factorial(4) */
+
+/* function iseven(x){
+    if (x % 2===0) {
+        prompt('true')
+    } else {
+        prompt('false')
+    }
+}
+
+iseven(4) */
+
+/* function text(x) {
+    return x*2;
+    console.log(x);
+    return x/2;
+}
+
+text(40); */
+
+/* function test(x,y) {
+    return y-x;
+}
+
+test(10,40); */
+
+/* function capitalize(str){ 
+    if (typeof str === "number") {
+        return "that is not a string!"
+    }
+    return str.charAt(0).toUppercase() + str.slice(1);
+    }
+
+    var city = "paris"; 
+    var capital = capitalize(city); */
+
+
+/* function square(x){
+    return x * x;
+}
+"4 Square is: " + square (4)
+var result = square(104); */
+
+/* function greet (person1, person2, person3 ) {
+    console.log("Hello " + person1);
+    console.log("Hello " + person2);
+    console.log("Hello " + person3);
+}
+greet("Ali","Zubair","Khan") 
+*/
+
+/* function argument(name) {
+    console.log("Hello" + name + " How's you doing");
+} 
+argument("Ali")
+*/
+
+/* function argument() {
+    console.log("HEllo there!");
+} 
+argument("khan")
+*/
+
+/* function hello() {
+    console.log("Hello!");
+    console.log("You there!");
+} 
+hello()
+*/
